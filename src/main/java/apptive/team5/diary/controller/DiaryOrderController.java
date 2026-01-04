ca4b7911b096bafb5779bb0a57aded819573eca5
sinsehwan <sk124590@gmail.com>
2026-01-04 17:42:36 +0900

    feat(diary): apply restController #39

--- src/main/java/apptive/team5/diary/controller/DiaryOrderController.java
+++ src/main/java/apptive/team5/diary/controller/DiaryOrderController.java
@@ -7,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/diaries/order")
 public class DiaryOrderController {
