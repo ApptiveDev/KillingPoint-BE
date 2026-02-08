95a902eb51f5da92e43320214180a53287efe03b
sinsehwan <sk124590@gmail.com>
2026-02-08 16:14:40 +0900

    feat(diary): modify domain to hard copy

--- src/main/java/apptive/team5/diary/domain/DiaryStoreEntity.java
+++ src/main/java/apptive/team5/diary/domain/DiaryStoreEntity.java
@@ -32,16 +32,53 @@ public class DiaryStoreEntity {
     )
     private UserEntity user;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(
-            name = "diary_id",
-            nullable = false,
-            foreignKey = @ForeignKey(name = "fk_diary_store_diary_id_ref_diary_id")
-    )
-    private DiaryEntity diary;
+    @Column(name = "diary_id", nullable = false)
+    private Long diaryId;
+
+    @Column(nullable = false)
+    private String musicTitle;
+
+    @Column(nullable = false)
+    private String artist;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String albumImageUrl;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String videoUrl;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DiaryScope scope;
+
+    @Column(nullable = false)
+    private String duration;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) DEFAULT '0'")
+    private String totalDuration;
+
+    @Column(nullable = false)
+    private String start;
+
+    @Column(nullable = false)
+    private String end;
 
     public DiaryStoreEntity(UserEntity user, DiaryEntity diary) {
         this.user = user;
-        this.diary = diary;
+        this.diaryId = diary.getId();
+
+        this.musicTitle = diary.getMusicTitle();
+        this.artist = diary.getArtist();
+        this.albumImageUrl = diary.getAlbumImageUrl();
+        this.videoUrl = diary.getVideoUrl();
+        this.content = diary.getContent();
+        this.scope = diary.getScope();
+        this.duration = diary.getDuration();
+        this.totalDuration = diary.getTotalDuration();
+        this.start = diary.getStart();
+        this.end = diary.getEnd();
     }
 }
