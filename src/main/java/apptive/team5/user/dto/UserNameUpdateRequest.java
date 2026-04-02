711527b8b793ffda958af50cd5c4048f078e2f8a
jinwon1234 <ljw0626@naver.com>
2026-04-02 16:10:56 +0900

    refactor: 이름 정책 변경

--- src/main/java/apptive/team5/user/dto/UserNameUpdateRequest.java
+++ src/main/java/apptive/team5/user/dto/UserNameUpdateRequest.java
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.Size;
 public record UserNameUpdateRequest(
 
         @NotBlank(message = "이름은 비어 있을 수 없습니다.")
-        @Size(min = 4, max = 10, message = "이름은 4자 이상 10자 미만이어야 합니다.")
-        @Pattern(regexp = "^[a-zA-Z가-힣]+$", message = "이름은 영어 또는 한글만 사용할 수 있습니다.")
+        @Size(min = 1, max = 20, message = "이름은 1자 이상 20자 이하여야 합니다.")
+        @Pattern(regexp = "^[a-zA-Z가-힣0-9 ]+$", message = "이름은 영어, 한글, 숫자, 공백만 사용할 수 있습니다.")
         String username
 
 ) {
